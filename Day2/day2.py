f9da9f55a1d61a569fe035224d46142d7ca25db6
Martin <marfur9@gmail.com>
2024-12-18 16:26:13 +0100

    days

--- Day2/day2.py
+++ Day2/day2.py
@@ -1,10 +1,14 @@
 inputfile = open('Day2/example.txt', 'r')
 sum=0
 
-prev=0
-direction="unsure"
+
 for line in inputfile:
+    i=0
+    prev=0
     for report in line.split():
-        if prev==0 and direction =="unsure":
+        if i==0:
             prev=report
+    
+
+        
 
