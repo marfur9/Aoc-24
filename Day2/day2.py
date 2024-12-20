19ce50d445129a84031425dd51bcdc248cd0ae08
Martin <marfur9@gmail.com>
2024-12-20 15:49:49 +0100

    day 2 prog

--- Day2/day2.py
+++ Day2/day2.py
@@ -5,9 +5,17 @@ sum=0
 for line in inputfile:
     i=0
     prev=0
+    direction=""
+    valid=True
     for report in line.split():
         if i==0:
             prev=report
+        elif i==1:
+            if report > prev:
+                direction="ascending"
+            elif report < prev:
+                direction="descending"
+        i = i + 1 
     
 
         
